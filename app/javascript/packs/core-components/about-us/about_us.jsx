dcbbc7c0717c9db1919a08330b3fbb5d7aa3013c
Prashant <prashant.badiger@cerner.com>
2018-12-02 12:17:43 +0530

    Added About Us and Terms and condition content

--- app/javascript/packs/core-components/about-us/about_us.jsx
+++ app/javascript/packs/core-components/about-us/about_us.jsx
@@ -5,8 +5,13 @@ export default class AboutUs extends React.Component {
 
     render(){
         return (
-            <div style={{margin: '20px 20px'}} >
+            <div><h2 style={{margin: '20px 20px'}} >
                 About Us
+            </h2>
+            <div style={{margin: '20px 20px'}}>
+            <p>Drive Aid Vehicle Services PVT LTD. </p>
+            <p># 1328, First Floor 24th A Main, 41st Cross, 9th Block Jaya Nagar</p>
+            <p>Bengaluru - 560041</p></div>
             </div>
         )
     }
--- app/javascript/packs/core-components/terms-and-conditions/terms_and_conditions.jsx
+++ app/javascript/packs/core-components/terms-and-conditions/terms_and_conditions.jsx
@@ -5,8 +5,32 @@ export default class TermsAndConditions extends React.Component {
 
     render(){
         return (
-            <div style={{margin: '20px 20px'}} >
-                Terms and conditions page
+            <div>
+                <h2 style={{margin: '20px 20px'}} >
+                    Terms and conditions
+                </h2>
+                <div>
+                    <ul>
+                    <li>All the minor breakdown services are fully free for subscribers.</li>
+                    <li>24/7 the services are available for Subscribers.</li>
+                    <li>We provide free service for major Breakdown such as Flat tyre, Battery jumpstart, and Starter problem if there is any equipment to be replacing i.e, battery, tyre, tube, Spark Plug, Wire Cable, and etc should be borne by Subscribers on Market Rate.</li>
+                    <li>DriveAid Covers on the standard geographical Region of Bangalore. Check(redirect to Coverage Area)</li>
+                    <li>If there are no chances to serve on Roadside for a major breakdown condition we will connect to nearest professional garage and charges applied on standard market price. </li>
+                    <li>In the case of Key Recovery Service there should be one extra key at your place and we just pick and handover the key to your preferred location.</li>
+                    <li>The Vehicle check up provides you the report of mainly needed garage service.</li>
+                    <li>If subscribed vehicle lost or met with accident we allow you to transfer your membership to new vehicle.</li>
+                    <li>Subscription plan cannot be activate immediately, it will take some time to activate for document and verification purpose and active confirmation  will be send you through mail or message.</li>
+                    <li>If anyone of the service is not used in a subscription period you are eligible to get 100% discount on renewal.</li>
+                    <li>There is no refund for one who has not used service in subscription period. We get you renewal for free. </li>
+                    <li>We provide maximum of two liters of fuel when fuel run out. The fuel and Can charges should be payable on market rate.</li>
+                    <li>Commute Service applicable only DriveAid coverage area and the charges should be depending on destination or Delivery location.</li>
+                    <li>Startup covers  i.e; (Carbon clean up and Petrol Wash)</li>
+                    <li>We do not guarantee to carry out the services if we are prevented from doing so in circumstances beyond our reasonable control including, without limitations, the actions of civil or government authorities, third party disputes, internal disputes, unavailability of spares, where we have taken reasonable steps to prevent the effects of such action on our services, but have been unable to do so; acts of God; terrorism or severe weather conditions.</li>
+                    <li>We do not guarantee to provide you the services in the case of any Event of Force Majeure and any failure to provide the services in such circumstances shall not constitute a breach of this Agreement on Our part.</li>
+                    <li>DriveAid reserves the right to cancel a membership if it becomes apparent that the member is abusing the roadside assistance service; if the member is using the services fraudulently.</li>
+                    <li>If the service you require is not provided for under this Agreement or is excluded by this Agreement, We will try, if you wish, to arrange it at your expense. We will charge you the applicable fee for our attendance and for the service provided. </li>
+                    </ul>
+                </div>
             </div>
         )
     }
